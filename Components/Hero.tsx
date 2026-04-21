34a8b7b71f4ca3b8c35b8d93ed1288f5eb10444d
Feivel Qutby <feivelqutby@gmail.com>
2026-04-21 20:51:28 +0700

    update boii

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -159,7 +159,7 @@ export default function Hero() {
                         lineHeight: 1.7,
                         maxWidth: 280,
                     }}>
-                        I build fast, accessible web apps. Currently open to new opportunities.
+                        I build fast, accessible web apps. Currently open to new opportunities. + gw jago
                     </p>
 
                     <div style={{
